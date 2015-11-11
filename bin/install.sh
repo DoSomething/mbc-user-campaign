7ca5cdac8da1f203695f7898dbe00db8b11299e5
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-11-11 17:58:34 -0500

    Composer install/uninstall scripts

---
+++ bin/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##
+# Installation script for mbp-userAPI-campaignActivity
+##
+
+# Assume messagebroker-config repo is one directory up
+cd ../messagebroker-config
+
+# Gather path from root
+MBCONFIG=`pwd`
+
+# Back to mbp-user-import
+cd ../mbp-userAPI-campaignActivity
+
+# Create SymLink for mbp-user-import application to make reference to for all Message Broker configuration settings
+ln -s $MBCONFIG .
